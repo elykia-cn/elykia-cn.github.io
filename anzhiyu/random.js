2efc03ec78a5a54871ac035e46a54108cd094352
Elysia <1815303900@qq.com>
2023-11-17 23:14:23 +0800

    Site updated: 2023-11-17 23:14:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/65c41b75.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/65c41b75.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
