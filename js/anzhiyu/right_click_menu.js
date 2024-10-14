f8f80402e7f0bd87d184e0eb8563de6fdd51328d
Elykia <elykia@qq.com>
2024-10-14 18:20:48 +0800

    Site updated: 2024-10-14 18:20:47

--- js/anzhiyu/right_click_menu.js
+++ js/anzhiyu/right_click_menu.js
@@ -363,7 +363,7 @@ function replaceAll(string, search, replace) {
 rm.searchBaidu = function () {
   anzhiyu.snackbarShow("即将跳转到必应搜索", false, 2000);
   setTimeout(function () {
-    window.open("https://www.bing.com/s?wd=" + selectTextNow);
+    window.open("https://cn.bing.com/search?q=6" + selectTextNow);
   }, "2000");
   rm.hideRightMenu();
 };
